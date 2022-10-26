f51fcc2708fbf927d4da60476a0aa4e5b1f7a868
veriszero <110993749+veriszero@users.noreply.github.com>
2022-10-26 17:26:12 -0300

    atualizacao 26102022

---
+++ aulasReact/aula2/aulaComponents/src/App.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './components/HelloWorld';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HelloWorld/>     
+      </header>
+      
+    </div>
+    
+  );
+}
+
+export default App;
