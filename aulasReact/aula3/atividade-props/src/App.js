f5e4cf155a7579996271fab5104882baab75c411
veriszero <110993749+veriszero@users.noreply.github.com>
2022-11-02 21:39:20 -0300

    atualizacao 021122

---
+++ aulasReact/aula3/atividade-props/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import TesteProps from './components/TesteProps';
+
+
+function App() {
+  return (
+    <div className="App">
+      <TesteProps nome = "Desenvolvimento Frontend"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ aulasReact/aula3/atividade-props/src/components/TesteProps.js
@@ -0,0 +1,9 @@
+function TesteProps(props){
+    return(
+        <div>
+            <p>Testando props no React, curso {props.nome} da Softex</p>
+        </div>
+    )
+}
+
+export default TesteProps
